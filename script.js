0ba3ce6ce1e7619e7ac30df9611498fc24b98085
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-11 22:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function updateTime() {
+    const now = new Date();
+    
+   
+    const dateTimeString = now.toLocaleString();
+
+    
+    document.getElementById("timer").innerText = dateTimeString;
+}
+
+
+updateTime();
+
+
+setInterval(updateTime, 1000);
